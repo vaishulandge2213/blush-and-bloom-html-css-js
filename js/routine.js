7b5f1193cbe8c0548b4b97d93fca6aa206096016
rutujaraut7499637769 <rutujar988@gmail.com>
2026-03-13 02:54:00 +0530

    clean code

--- js/routine.js
+++ js/routine.js
@@ -8,24 +8,19 @@ window.location.href="night.html";
 
 function done(btn){
 
-// button change
 btn.innerHTML="✔ Done";
 btn.style.background="green";
 btn.disabled = true;
 
-// sagle buttons select kara
 let buttons = document.querySelectorAll(".card button");
 
 let allDone = true;
 
-// check kara sagle done zale ka
 buttons.forEach(function(b){
 if(!b.disabled){
 allDone = false;
 }
 });
-
-// jar sagle done zale tar alert
 if(allDone){
 alert("Your skincare is done!");
 }
