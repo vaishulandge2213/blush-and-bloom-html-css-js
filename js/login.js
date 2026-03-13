bf929f007c0b1d02f5e1672d39bc9f81ba7f8f18
Supriya-004 <supriyawalhekar08@gmail.com>
2026-03-13 15:42:59 +0530

    alert message added

--- js/login.js
+++ js/login.js
@@ -3,35 +3,13 @@ function login(event) {
 
     let email = document.getElementById("email").value.trim();
     let password = document.getElementById("password").value.trim();
-    let confirmPassword = document.getElementById("confirmPassword").value.trim();
-    let message = document.getElementById("message");
 
-    if (email === "" || password === "" || confirmPassword === "") {
-        message.style.color = "red";
-        message.innerText = "Please fill all fields!";
+    if (email === "" || password === "") {
+        alert("Please fill all fields!");
         return;
     }
-
-    if (password.length < 6) {
-        message.style.color = "red";
-        message.innerText = "Password must be at least 6 characters!";
-        return;
-    }
-
-  
-    if (password !== confirmPassword) {
-        message.style.color = "red";
-        message.innerText = "Passwords do not match!";
-        return;
-    }
-
     localStorage.setItem("userEmail", email);
-    localStorage.setItem("userPassword", password);
 
-    message.style.color = "green";
-    message.innerText = "Login Successfully!";
-
-    setTimeout(function () {
-        window.location.href = "../index.html";
-    }, 1000);
+    alert("Login Successfully!");
+    window.location.href = "../index.html";
 }
